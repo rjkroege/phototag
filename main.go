7f627d652349498ab6e14a7ba30ca36e8febfa8b
Robert Kroeger <rjkroege@liqui.org>
2021-08-16 20:19:55 -0400

    Added date range extraction

--- main.go
+++ main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"log"
-	"encoding/json"
 	"encoding/csv"
+	"encoding/json"
 	"flag"
-	"time"
-	"io"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 )
 
 // App to make takeout .json into .csv
 
-
 type LocationValue struct {
 	longitude float64
-	latitude float64
+	latitude  float64
 	timestamp time.Time
 }
 
 type LocationSample struct {
 	TimestampMs string `json:timestampMs`
-	LatitudeE7 int `json:latitudeE7`
-	LongitudeE7 int `json:longitudeE7`
-	Accuracy int `json:accuracy`
-	Source string `json:source` 
+	LatitudeE7  int    `json:latitudeE7`
+	LongitudeE7 int    `json:longitudeE7`
+	Accuracy    int    `json:accuracy`
+	Source      string `json:source`
 }
 
 type AllLocations struct {
@@ -40,13 +40,14 @@ func parseJsonFile(fd io.Reader) ([]LocationSample, error) {
 	if err := decoder.Decode(&recs); err != nil {
 		return nil, fmt.Errorf("can't decode file: %v", err)
 	}
-	
+
 	return recs.Locations, nil
 }
 
-
-func convertFormat(recs []LocationSample) []LocationValue {
+func convertFormat(recs []LocationSample, starttime, endtime time.Time) []LocationValue {
 	processed := make([]LocationValue, 0, len(recs))
+
+	log.Println(starttime, endtime)
 
 	for i, ls := range recs {
 		// Time
@@ -55,11 +56,18 @@ func convertFormat(recs []LocationSample) []LocationValue {
 			log.Printf("can't parse [%d]%s: %v", i, ls.TimestampMs, err)
 			continue
 		}
-		tm := time.Unix(0, ts * 1000 * 1000)
+		tm := time.Unix(0, ts*1000*1000)
+
+		if !tm.After(starttime) {
+			continue
+		}
+		if tm.After(endtime) {
+			break
+		}
 
 		processed = append(processed, LocationValue{
 			timestamp: tm,
-			latitude: float64(ls.LatitudeE7) / 10e6,
+			latitude:  float64(ls.LatitudeE7) / 10e6,
 			longitude: float64(ls.LongitudeE7) / 10e6,
 		})
 	}
@@ -67,14 +75,42 @@ func convertFormat(recs []LocationSample) []LocationValue {
 	return processed
 }
 
-// TODO(rjk): add support daterange
+const daterangeformat = "20060102"
+
+func parseDateRange(dr string) (time.Time, time.Time, error) {
+	drs := strings.Split(dr, "-")
+
+	if len(drs) != 2 {
+		return time.Time{}, time.Time{}, fmt.Errorf("can't parse daterange %s", dr)
+	}
+
+	starttime, err := time.Parse(daterangeformat, drs[0])
+	if err != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("can't parse daterange %s: %v", dr, err)
+	}
+
+	endtime, err := time.Parse(daterangeformat, drs[1])
+	if err != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("can't parse daterange %s: %v", dr, err)
+	}
+
+	return starttime, endtime, nil
+}
+
+var daterange = flag.String("d", "19000101-21001231", "[year month date - year month date) e.g. 20170101-20171241")
 
 func main() {
 	log.Println("hello")
 
-	// insert options here as needed
+	// Setup options before calling Parse()
 	flag.Parse()
-	
+
+	starttime, endtime, err := parseDateRange(*daterange)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(starttime.String(), endtime.String())
+
 	// for each arg
 	for _, f := range flag.Args() {
 		log.Println(f)
@@ -83,7 +119,7 @@ func main() {
 			log.Printf("can't open %q: %v", f, err)
 			continue
 		}
-		
+
 		recs, err := parseJsonFile(fd)
 		if err != nil {
 			log.Printf("can't parse %q: %v", f, err)
@@ -96,10 +132,12 @@ func main() {
 			log.Println(i, recs[i])
 		}
 
-		locations := convertFormat(recs)
+		locations := convertFormat(recs, starttime, endtime)
 
-		for i := 0; i < 10 && i < len(locations); i++ {
-			log.Printf("[%d] %s %f %f" , i , locations[i].timestamp.String(), locations[i].latitude, locations[i].longitude)
+		for i, loc := range locations {
+			if loc.timestamp.After(starttime) && loc.timestamp.Before(endtime) {
+				log.Printf("[%d] %s %f %f", i, loc.timestamp.String(), loc.latitude, loc.longitude)
+			}
 		}
 
 		of := f + ".csv"
@@ -109,7 +147,7 @@ func main() {
 			continue
 		}
 
-		if err := convertToCsv(ofd, locations); err != nil {
+		if err := convertToCsv(ofd, locations, starttime, endtime); err != nil {
 			log.Println("convertToCsv failed:", err)
 			ofd.Close()
 			continue
@@ -119,7 +157,7 @@ func main() {
 
 }
 
-func convertToCsv(ofd io.Writer, locs []LocationValue) error {
+func convertToCsv(ofd io.Writer, locs []LocationValue, starttime, endtime time.Time) error {
 	csvout := csv.NewWriter(ofd)
 
 	// write me a header line
@@ -134,21 +172,22 @@ func convertToCsv(ofd io.Writer, locs []LocationValue) error {
 	}
 
 	for i, loc := range locs {
-		if err := csvout.Write([]string{
-			fmt.Sprintf("%d", i + 1),
-			fmt.Sprintf("%f", loc.latitude),
-			fmt.Sprintf("%f", loc.longitude),
-			loc.timestamp.UTC().Format("2006/01/02"),
-			loc.timestamp.UTC().Format("15:04:05.000"),
-		}); err != nil {
-			return fmt.Errorf("convertToCsv write: %v", err)
+		if loc.timestamp.After(starttime) && loc.timestamp.Before(endtime) {
+
+			if err := csvout.Write([]string{
+				fmt.Sprintf("%d", i+1),
+				fmt.Sprintf("%f", loc.latitude),
+				fmt.Sprintf("%f", loc.longitude),
+				loc.timestamp.UTC().Format("2006/01/02"),
+				loc.timestamp.UTC().Format("15:04:05.000"),
+			}); err != nil {
+				return fmt.Errorf("convertToCsv write: %v", err)
+			}
 		}
 	}
 	csvout.Flush()
 	return csvout.Error()
 }
-
-
 
 /*
 
@@ -180,7 +219,7 @@ alt =      Elevation (in meters) of the point. Add "ft" or "feet" for feet.
       diff =     Geocache difficulty
       ele =      Elevation (in meters) of the point. Add "ft" or "feet" for feet.
       e/w =      'e' for eastern hemisphere, 'w' for western
-      exported = Geocache export date 
+      exported = Geocache export date
       found =    Geocache last found date
       fix =      3d, 2d, etc.
       gcid =     Geocache cache id
@@ -222,7 +261,7 @@ alt =      Elevation (in meters) of the point. Add "ft" or "feet" for feet.
       y =        Latitude
       y_pos =    Latitude
       z =        Altitude (elevation).  See "elevation".
-   
+
 No,Latitude,Longitude,Name,Description,Symbol,Date,Time,Format
 1,51.075139,12.463689,"578","578","Waypoint",2005/04/26,16:27:23,"gdb"
 2,51.081104,12.465277,"579","579","Waypoint",2005/04/26,16:27:23,"gdb"
